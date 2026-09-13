45f04534009f56a528b00336f8a3a6652daf2d9d
Benyamin Suicmez <b.suicmez@gmx.de>
2026-09-13 13:16:25 +0200

    Add board initialization with random cell states

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello Github")
+import numpy as np
+
+
+# Creating the game board
+board = np.random.choice((0,1), size=(30, 30), p=[0.7, 0.3])
+
+# Creating a second, temporary board
+temp_board = np.zeros_like(board)
+
+
+
+print(board)
+
+# dont have much time => must speedcode it in a few hours :O
